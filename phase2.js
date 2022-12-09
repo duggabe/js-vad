eb7df1abca72e4fa31bae152285484a8f555aa6a
duggabe <barry@dcsmail.net>
2022-12-09 16:08:22 -0600

    revise phase2.js after testing with MediaWiki

--- phase2.js
+++ phase2.js
@@ -1,38 +1,38 @@
-/* phase2.js */
+"use strict"
 
-//  changed <article> to <span>
-//  changed <section> to <div>
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+console.log ("Version-adaptive documentation");
 
 var gr_version = "";
 
-document.body.onload = function() {phase2()};
-
-function phase2()
-    {
     var w_loc = window.location.href;
-    const url = new URL(w_loc);
-    console.log ("phase2()", w_loc);
-    var p1 = url.searchParams.get('ver1');
-    var p2 = url.searchParams.get('ver2');
-    var p3 = url.searchParams.get('ver3');
+    const url1 = new URL(w_loc);
+    var url2 = url1.toString();
+    var url = url2.replace("?title=","/");
+    console.log ("phase2()", url);
+    var p1 = url1.searchParams.get('ver1');
+    var p2 = url1.searchParams.get('ver2');
+    var p3 = url1.searchParams.get('ver3');
     console.log (p1,p2,p3);
     if ((p1 == null) && (p2 == null) && (p3 == null))
         {       // first pass
         var _art = document.getElementsByTagName("span");
         if (_art.length > 0)    // found <span>
             {
-            console.log ("<span> found");
+            console.log (_art.length, "spans found");
             var _art1 = _art[0].getAttribute("data-vad");
             if (_art1 == "gr_vad")
                 {
                 console.log ("found data-vad tag in <span>");
-                var _nav = document.getElementsByClassName("vector-menu-content-list");
+                var _nav = document.getElementById("p-tb")
                 var _br = document.createElement("br");
-                _nav[0].appendChild (_br);
+                _nav.appendChild (_br);
                 var _li1 = document.createElement("li");
                 _li1.innerHTML = "<br>Customize this page:";
+                // _li1.setAttribute("class", "mw-list-item");
                 var _form1 = document.createElement("form");
-               _form1.setAttribute("action", "#");       // action
+               _form1.setAttribute("action", url);       // action
 // ver1 3.8
                 var _inp1 = document.createElement("input");
                 _inp1.setAttribute("type", "checkbox");
@@ -42,10 +42,8 @@ function phase2()
                 _form1.appendChild (_inp1);
                 var _lbl1 = document.createElement("label");
                 _lbl1.setAttribute("for", "_ver1");
-                _lbl1.innerHTML = " 3.8";
+                _lbl1.innerHTML = " 3.8<br>";
                 _form1.appendChild (_lbl1);
-                var _nbsp = document.createTextNode("  ");
-                _form1.appendChild (_nbsp);
 // ver2 3.9
                 var _inp2 = document.createElement("input");
                 _inp2.setAttribute("type", "checkbox");
@@ -55,9 +53,8 @@ function phase2()
                 _form1.appendChild (_inp2);
                 var _lbl2 = document.createElement("label");
                 _lbl2.setAttribute("for", "_ver2");
-                _lbl2.innerHTML = " 3.9";
+                _lbl2.innerHTML = " 3.9<br>";
                 _form1.appendChild (_lbl2);
-                _form1.appendChild (_nbsp);
 // ver3 3.10
                 var _inp3 = document.createElement("input");
                 _inp3.setAttribute("type", "checkbox");
@@ -67,9 +64,8 @@ function phase2()
                 _form1.appendChild (_inp3);
                 var _lbl3 = document.createElement("label");
                 _lbl3.setAttribute("for", "_ver3");
-                _lbl3.innerHTML = " 3.10";
+                _lbl3.innerHTML = " 3.10<br>";
                 _form1.appendChild (_lbl3);
-                _form1.appendChild(_br);
                 _form1.appendChild(_br);
 // submit
                 var _inp4 = document.createElement("input");
@@ -77,7 +73,7 @@ function phase2()
                 _inp4.setAttribute("value", "Submit");
                 _form1.appendChild (_inp4);
                 _li1.appendChild (_form1);
-                _nav[0].appendChild (_li1);
+                _nav.appendChild (_li1);
                 }   // found data-vad tag in <span>
             }   // end found <span>
         }   // first pass
@@ -105,78 +101,80 @@ function phase2()
             var temp = gr_version.slice (0, ver_len-2);
             gr_version = temp;
             }
-        var _nav = document.getElementsByClassName("vector-menu-content-list");
+        var _nav = document.getElementById("p-tb")
         var _br = document.createElement("br");
-        _nav[0].appendChild (_br);
+        _nav.appendChild (_br);
         var _li1 = document.createElement("li");
         _li1.innerHTML = "<br>Customize this page:";
         var _form1 = document.createElement("form");
-        _form1.setAttribute("action", "#");       // action
+        _form1.setAttribute("action", url);       // action
 // ver1 3.8
-                var _inp1 = document.createElement("input");
-                _inp1.setAttribute("type", "checkbox");
-                _inp1.setAttribute("id", "_ver1");
-                _inp1.setAttribute("name", "ver1");
-                _inp1.setAttribute("value", "3.8");
-                if ((gr_version.indexOf ("3.8")) != -1)      // found
-                    _inp1.setAttribute ("checked", "true");
-                _form1.appendChild (_inp1);
-                var _lbl1 = document.createElement("label");
-                _lbl1.setAttribute("for", "_ver1");
-                _lbl1.innerHTML = " 3.8";
-                _form1.appendChild (_lbl1);
-                var _nbsp = document.createTextNode("  ");
-                _form1.appendChild (_nbsp);
+        var _inp1 = document.createElement("input");
+        _inp1.setAttribute("type", "checkbox");
+        _inp1.setAttribute("id", "_ver1");
+        _inp1.setAttribute("name", "ver1");
+        _inp1.setAttribute("value", "3.8");
+        if ((gr_version.indexOf ("3.8")) != -1)      // found
+            _inp1.setAttribute ("checked", "true");
+        _form1.appendChild (_inp1);
+        var _lbl1 = document.createElement("label");
+        _lbl1.setAttribute("for", "_ver1");
+        _lbl1.innerHTML = " 3.8<br>";
+        _form1.appendChild (_lbl1);
 // ver2 3.9
-                var _inp2 = document.createElement("input");
-                _inp2.setAttribute("type", "checkbox");
-                _inp2.setAttribute("id", "_ver2");
-                _inp2.setAttribute("name", "ver2");
-                _inp2.setAttribute("value", "3.9");
-                if ((gr_version.indexOf ("3.9")) != -1)      // found
-                    _inp2.setAttribute ("checked", "true");
-                _form1.appendChild (_inp2);
-                var _lbl2 = document.createElement("label");
-                _lbl2.setAttribute("for", "_ver2");
-                _lbl2.innerHTML = " 3.9";
-                _form1.appendChild (_lbl2);
-                _form1.appendChild (_nbsp);
+        var _inp2 = document.createElement("input");
+        _inp2.setAttribute("type", "checkbox");
+        _inp2.setAttribute("id", "_ver2");
+        _inp2.setAttribute("name", "ver2");
+        _inp2.setAttribute("value", "3.9");
+        if ((gr_version.indexOf ("3.9")) != -1)      // found
+            _inp2.setAttribute ("checked", "true");
+        _form1.appendChild (_inp2);
+        var _lbl2 = document.createElement("label");
+        _lbl2.setAttribute("for", "_ver2");
+        _lbl2.innerHTML = " 3.9<br>";
+        _form1.appendChild (_lbl2);
 // ver3 3.10
-                var _inp3 = document.createElement("input");
-                _inp3.setAttribute("type", "checkbox");
-                _inp3.setAttribute("id", "_ver3");
-                _inp3.setAttribute("name", "ver3");
-                _inp3.setAttribute("value", "3.10");
-                if ((gr_version.indexOf ("3.10")) != -1)      // found
-                    _inp3.setAttribute ("checked", "true");
-                _form1.appendChild (_inp3);
-                var _lbl3 = document.createElement("label");
-                _lbl3.setAttribute("for", "_ver3");
-                _lbl3.innerHTML = " 3.10";
-                _form1.appendChild (_lbl3);
-                _form1.appendChild(_br);
-                _form1.appendChild(_br);
+        var _inp3 = document.createElement("input");
+        _inp3.setAttribute("type", "checkbox");
+        _inp3.setAttribute("id", "_ver3");
+        _inp3.setAttribute("name", "ver3");
+        _inp3.setAttribute("value", "3.10");
+        if ((gr_version.indexOf ("3.10")) != -1)      // found
+            _inp3.setAttribute ("checked", "true");
+        _form1.appendChild (_inp3);
+        var _lbl3 = document.createElement("label");
+        _lbl3.setAttribute("for", "_ver3");
+        _lbl3.innerHTML = " 3.10<br>";
+        _form1.appendChild (_lbl3);
+        _form1.appendChild(_br);
 // submit
-                var _inp4 = document.createElement("input");
-                _inp4.setAttribute("type", "submit");
-                _inp4.setAttribute("value", "Submit");
-                _form1.appendChild (_inp4);
-                _li1.appendChild (_form1);
-                _nav[0].appendChild (_li1);
-//
+        var _inp4 = document.createElement("input");
+        _inp4.setAttribute("type", "submit");
+        _inp4.setAttribute("value", "Submit");
+        _form1.appendChild (_inp4);
+        _li1.appendChild (_form1);
+        _nav.appendChild (_li1);
+
         if (gr_version.length > 0)
             {
             var element = document.getElementsByTagName("div");
-            for (let i = 0; i < element.length; i++)
+            var _len = element.length;
+            console.log (_len, "divs found");
+            var k;
+            for (k = 0; k < _len; k++)
                 {
-                var _ver = element[i].getAttribute("data-vad");
-                console.log ("div version is ", _ver);
-                if ((gr_version.indexOf (_ver)) == -1)      // not found
+                var _ver = element[k].getAttribute("data-vad");
+                if (_ver != null)
                     {
-                    element[i].setAttribute ("hidden", "true");
+                    console.log ("div version is ", _ver);
+                    if ((gr_version.indexOf (_ver)) == -1)      // not found
+                        {
+                        element[k].setAttribute ("hidden", "true");
+                        }
                     }
                 }
-            }   // end have selections
+            }
         }   // second pass
-    }
+
 
